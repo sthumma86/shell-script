d52f1e397a0814e17b3966c4748f0c88d5f9f2d8
sthumma86 <sthummacld86@gmail.com>
2024-03-28 15:25:49 -0400

    modfied script

--- install-mysql.sh
+++ install-mysql.sh
@@ -4,11 +4,28 @@ ID=$(id -u)
 
 if [ $ID ne 0 ] 
 then
-    echo "Error: please execute with root user"
+    echo "Error: please run this script with root user"
     exit 1
 else
     echo "You are root user, proceeding with installation"
 fi
 
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: Installing MySQL is failed"
+    exit 1
+else
+    echo "Installing MySQL is SUCCESS"
+fi
+
 yum install git -y
 
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: Installing GIT is failed"
+    exit 1
+else
+    echo "Installing GIT is SUCCESS"
+fi
