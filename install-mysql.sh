835e170dd56cbeab73cd32b8007091ac2880bd44
sthumma86 <sthummacld86@gmail.com>
2024-03-28 14:50:00 -0400

    modfied script

--- install-mysql.sh
+++ install-mysql.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+ID=$(id -u)
 
-if [ id ne 0] then
+if [ ID ne 0 ] 
+then
     echo "Error: please execute with root user"
+    exit 1
 else
     echo "proceeding with installation"
 fi
 
 VALIDATE ()
     {
-        if [ $? ne 0 ] then
-            echo "Error: Installation is not succesful"
+        if [ $? ne 0 ] 
+        then
+            echo "Error: Installation is not successful"
         else
             echo "Installation is success"
+        fi
     }
 
 yum install mysql -y
